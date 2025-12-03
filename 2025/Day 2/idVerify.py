e7c036505c79ee1c54791cab1d1de817a25331c6
DanyConti <daniele.conti03@universitadipavia.it>
2025-12-03 16:01:56 +0100

    Year 2025 Day 2 Star 2 Completed

--- 2025/Day 2/idVerify.py
+++ 2025/Day 2/idVerify.py
@@ -8,20 +8,35 @@ with open(FILE, "r") as f:
     file = f.read()
     file = file.replace(",", "\n").splitlines()
 
-for line in file:
+for line in file: # prende il range
     line = line.strip(",")
-    first, last = map(int, line.split("-"))
-    
-    check = 0
-    for i in range(first, last+1):
+    first, last = map(int, line.split("-"))    
+    for i in range(first, last+1): # ruota il range per ogni numero
         i = str(i)
         
-        length = len(i) // 2
-        before = i[:length]
-        after = i[length:]
-        
-        if before == after:
-            numbers.append(int(i))
+        for lap in range(2, len(i) + 1): # ruotare il controllo e il controllato  
+            #print("lap: " + str(lap))  
+            if len(i) % lap != 0:
+                pass
+            else:
+                length = len(i) // lap
+                
+                #print(length)
+                
+                before = i[:length]
+                after = i[length:]
+                #print(before, after, len(after))
+                
+                check = 0
+                for every in range(0, len(after), length):
+                    #print("every: " + str(every))
+                    if before == after[every : every + length]:
+                        check += 1
+                    #print("prova: " + after[every : every + length])
+                
+                if check + 1 == lap:
+                    numbers.append(int(i))
+                    break
 
 sum = 0        
 for i in numbers:
